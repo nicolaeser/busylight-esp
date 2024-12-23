086a4de44a9110553cb32d43349e66d3f8cc8bce
iGoX <code@igox.org>
2024-12-23 15:57:19 +0100

    README - Update install section (WiFi config) (#4)

--- ESP32/boot.py
+++ ESP32/boot.py
@@ -1,21 +1,20 @@
 # This file is executed on every boot (including wake-boot from deepsleep)
-# It is executed after boot.py, before main.py
+# It is executed before main.py
 import os, machine
 import network
 import gc
 
 gc.collect()
 
-# Disable Access Point
-ap_if = network.WLAN(network.AP_IF)
-ap_if.active(False)
 
-# Connect to the WIFI when booting
+# --- WIFI Configuration ---
 SSID = '<YOUR WIFI SSID>'               # Set the WIFI network SSID
 PASSWORD = '<YOUR WIFI PASSWORD>'       # Set the WIFI network password
 network.country('<YOUR COUNTRY CODE>')  # Set the country code for the WIFI (ISO 3166-1 Alpha-2 country code)
 network.hostname('busylight-esp32')     # Hostname that will identify this device on the network
+# --------------------------
 
+# Function to connect to the WIFI
 def boot_wifi_connect():
     wlan = network.WLAN(network.STA_IF)
     if not wlan.isconnected():
@@ -25,10 +24,13 @@ def boot_wifi_connect():
         while not wlan.isconnected():
             pass
     ip, mask, gateway, dns = wlan.ifconfig()
+    
+    # Print the network configuration
     print('\nNetwork config:')
     print('- IP address: ' + ip)
     print('- Network mask: ' + mask)
     print('- Network gateway: ' + gateway)
     print('- DNS server: ' + dns + '\n')
 
+# Connect to the WIFI when booting
 boot_wifi_connect()
